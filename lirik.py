32ca3877115987e6179dc4ffd53bf9bf2da01e86
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-03-29 02:22:56 +0700

    save

--- lirik.py
+++ lirik.py
@@ -3,22 +3,14 @@ from threading import Thread
 import sys
 
 lyrics = [
-    ("Di hahurangan ki", 0.6),
-    ("Ai hodo patikkoson au", 0.7),
-    ("Molo borngin lasni ari", 0.6),
-    ("Ingot do ho tu au", 0.8),
-    ("", 0.5),
-    ("Mauliate ma inang", 0.6),
-    ("Di sude haholongan mi", 0.7),
-    ("Inanghu naburju na uli lagu", 0.8),
-    ("", 0.5),
-    ("Di tangiangmi i inang", 0.6),
-    ("Di haholongan mi tu au", 0.7),
-    ("Apus ma ilumi inang", 0.6),
-    ("Posroham posroham ale inang", 0.9)
+    ("Di hahurangan ki", 0.1),
+    ("Ai hodo patikkoson au", 0.2),
+    ("Molo borngin lasni ari", 0.15),
+    ("Ingot do ho tu au", 0.3), 
+    
 ]
 
-delays = [0, 5.0, 11.0, 17.0, 20.8, 0, 5.0, 11.0, 17.0, 20.8, 0 , 0 , 0]
+delays = [0, 1.7, 6.0, 10.0]
 
 def animate_text(text, delay=0.1):
     for char in text:
